c27880c78ed20e9f82015e79151e42d9a9899ea4
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-22 17:21:47 +0100

    redirecting to a browse page with a header

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
-import { Loading } from '../components';
+import { Loading, Header } from '../components';
 
 export function BrowserContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -16,5 +16,14 @@ export function BrowserContainer({ slides }) {
     }, 3000);
   }, [profile, profile.displayName]);
 
-  return profile.displayName ? loading ? <Loading src={user.photoURL} /> : null : <SelectProfileContainer user={user} setProfile={setProfile} />;
+  return profile.displayName ? (
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="joker1">
+        <p>Hello</p>
+      </Header>
+    </>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
