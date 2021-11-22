f741b47fea5c009ad8de959a2801cc0d19cc90b2
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-22 15:35:34 +0100

    adding another custom hook in order to pull out the data

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
-  return <p>Hello from Browse</p>;
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  console.log(series);
+  console.log(films);
+  return (
+    <>
+      <p>Hello from Browse</p>
+    </>
+  );
 }
